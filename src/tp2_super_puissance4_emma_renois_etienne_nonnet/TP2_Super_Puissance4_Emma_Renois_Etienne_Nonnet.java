5b83393352df616b24cd08d2d2b12e5367d52866
renois <renois@LAPTOP-F5QBNG03.home>
2020-11-04 17:14:15 +0100

    Création du dépot

--- src/tp2_super_puissance4_emma_renois_etienne_nonnet/TP2_Super_Puissance4_Emma_Renois_Etienne_Nonnet.java
+++ src/tp2_super_puissance4_emma_renois_etienne_nonnet/TP2_Super_Puissance4_Emma_Renois_Etienne_Nonnet.java
@@ -4,6 +4,7 @@
  */
 package tp2_super_puissance4_emma_renois_etienne_nonnet;
 
+import java.util.Scanner;
 /**
  *
  * @author Etienne Nonnet
@@ -14,6 +15,20 @@ public class TP2_Super_Puissance4_Emma_Renois_Etienne_Nonnet {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        String x;
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Entrer nom Joueur1 : ");
+        x = sc.next();
+        Joueur Joueur1 = new Joueur (x);
+        System.out.println("Le Joueur1 s'appelle : "+Joueur1.Nom+"\n");
+        
+        String y;
+        Scanner sc2 = new Scanner (System.in);
+        System.out.println("Entrer nom Joueur2 : ");
+        y = sc2.next();
+        Joueur Joueur2 = new Joueur (y);
+        System.out.println("Le Joueur2 s'appelle : "+Joueur2.Nom+"\n");
+        
         // Main du code (relit toutes les classes entre elles)
         Jeton j1 = new Jeton("rouge");
         //System.out.println(j1.lireCouleur());
