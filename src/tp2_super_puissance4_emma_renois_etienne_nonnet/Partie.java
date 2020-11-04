7bb8ddc4175203ffdbe19013d4425567f8d7d4a0
renois <renois@LAPTOP-F5QBNG03.home>
2020-11-04 16:22:29 +0100

    Création du dépot

--- src/tp2_super_puissance4_emma_renois_etienne_nonnet/Partie.java
+++ src/tp2_super_puissance4_emma_renois_etienne_nonnet/Partie.java
@@ -15,12 +15,27 @@ public class Partie {
         double x = Math.random();
         if (x == 0){
             ListeJoueurs[0].affecterCouleur("rouge");
+            for (int i=0; i<21; i++){
+                Jeton J1 = new Jeton("rouge");
+                ListeJoueurs[0].ajouterJeton(J1);
+            }
             ListeJoueurs[1].affecterCouleur("jaune");
-            
+            for (int i=0; i<21; i++){
+                Jeton J2 = new Jeton("jaune");
+                ListeJoueurs[1].ajouterJeton(J2);
+            }
         }
         else{
             ListeJoueurs[0].affecterCouleur("jaune");
+            for (int i=0; i<21; i++){
+                Jeton J1 = new Jeton("jaune");
+                ListeJoueurs[0].ajouterJeton(J1);
+            }
             ListeJoueurs[1].affecterCouleur("rouge");
+            for (int i=0; i<21; i++){  
+                Jeton J2 = new Jeton("rouge");
+                ListeJoueurs[1].ajouterJeton(J2);
+            }
         }
     }
     
@@ -38,7 +53,8 @@ public class Partie {
             int o = (int)(Math.random() * 5);
             int p = (int)(Math.random() * 6);
             GrilleJeu[0].placerDesintegrateur(o,p);
-    }
+        }
+        
         
     }
     
